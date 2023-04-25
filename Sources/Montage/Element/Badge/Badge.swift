5bd7be70e17ec4e1ab14078a5cac3a37b260ddeb
Euigyom Kim <egkim@dehol.kr>
2023-04-25 10:36:24 +0900

    feat: Content Badge 파일 추가
    Signed-off-by: Euigyom Kim <egkim@dehol.kr>

---
+++ Sources/Montage/Element/Badge/Badge.swift
@@ -0,0 +1,10 @@
+//
+//  Badge.swift
+//  Montage
+//
+//  Created by Euigyom Kim on 2023/04/24.
+//
+
+import Foundation
+
+public enum Badge {}
---
+++ Sources/Montage/Element/Badge/Content/ContentBadge.swift
@@ -0,0 +1,14 @@
+//
+//  ContentBadge.swift
+//  Montage
+//
+//  Created by Euigyom Kim on 2023/04/24.
+//
+
+import Foundation
+
+extension Badge {
+    public class Content: UIView {
+        
+    }
+}
