09db36232e8133dbb51e334b357402dda6966ccf
Euigyom Kim <egkim@dehol.kr>
2023-04-27 09:41:00 +0900

    WIP: 푸시 뱃지 소스 파일 생성
    Signed-off-by: Euigyom Kim <egkim@dehol.kr>

---
+++ Sources/Montage/Element/Badge/Push/PushBadge.swift
@@ -0,0 +1,16 @@
+//
+//  PushBadge.swift
+//  Montage
+//
+//  Created by Euigyom Kim on 2023/04/26.
+//
+
+import UIKit
+
+extension Badge {
+    /// 특정 요소에 대한 알림을 표시할 수 있는 뱃지입니다
+    public class Push: UIView {
+        
+    }
+}
+
---
+++ Sources/Montage/Element/Badge/Push/PushBadgeController.swift
@@ -0,0 +1,8 @@
+//
+//  PushBadgeController.swift
+//  Montage
+//
+//  Created by Euigyom Kim on 2023/04/26.
+//
+
+import Foundation
