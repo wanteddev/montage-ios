7a4f1cd644723f8e33bb306686b0498e2f4b2184
Euigyom Kim <deholexp@gmail.com>
2023-04-27 16:57:14 +0900

    feat: 푸시 뱃지 구현

--- Sources/Montage/Element/Badge/Push/PushBadge.swift
+++ Sources/Montage/Element/Badge/Push/PushBadge.swift
@@ -6,11 +6,168 @@
 //
 
 import UIKit
+import Pretendard
 
 extension Badge {
     /// 특정 요소에 대한 알림을 표시할 수 있는 뱃지입니다
     public class Push: UIView {
+        private enum Const {
+            static let defaultDotSize: CGSize = .init(width: 4, height: 4)
+        }
         
+        public enum Varient: Equatable {
+            case dot, new, number(Int)
+        }
+        
+        public var varient: Varient = .dot {
+            didSet {
+                setupUpdateableConstraints()
+                updateViews()
+            }
+        }
+        
+        private lazy var stackView = UIStackView()
+        private lazy var textLabel = UILabel()
+        
+        private var stackViewConstraints: [NSLayoutConstraint] = []
+        
+        /// Push 뱃지 객체를 생성합니다.
+        public init() {
+            super.init(frame: .zero)
+            
+            setupViews()
+        }
+        
+        public required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            
+            setupViews()
+        }
+        
+        public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+            super.traitCollectionDidChange(previousTraitCollection)
+            
+            updateViews()
+        }
+        
+        public override func layoutSubviews() {
+            super.layoutSubviews()
+            
+            setupLayer()
+        }
+        
+        /// Element의 기본적인 사이즈를 정의합니다.
+        override public var intrinsicContentSize: CGSize {
+            guard let text = getAttributedText() else { return Const.defaultDotSize }
+            
+            let textSize = text.size()
+            let edgeInsets = varient.edgeInsets
+            
+            return .init(
+                width: textSize.width + edgeInsets.horizontal,
+                height: textSize.height + edgeInsets.vertical
+            )
+        }
     }
 }
 
+extension Badge.Push {
+    private func setupViews() {
+        addSubview(stackView)
+        
+        setupStackView()
+        setupUpdateableConstraints()
+        
+        updateViews()
+    }
+    
+    private func setupStackView() {
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.addArrangedSubview(textLabel)
+    }
+    
+    private func setupUpdateableConstraints() {
+        setupStackViewConstraints()
+        updateConstraints()
+        setupLayer()
+    }
+    
+    private func setupStackViewConstraints() {
+        NSLayoutConstraint.deactivate(stackViewConstraints)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let insets = varient.edgeInsets
+        
+        let constraints = [
+            stackView.leftAnchor.constraint(equalTo: leftAnchor, constant: insets.left),
+            stackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -insets.right),
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: insets.top),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -insets.bottom),
+            stackView.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+        stackViewConstraints = constraints
+    }
+    
+    private func setupLayer() {
+        layer.cornerRadius = frame.height / 2
+        layer.masksToBounds = true
+    }
+}
+
+extension Badge.Push {
+    private func updateViews() {
+        updateColor()
+        updateTextLabel()
+        invalidateIntrinsicContentSize()
+    }
+    
+    private func updateColor() {
+        backgroundColor = .alias(.primaryNormal)
+    }
+    
+    private func updateTextLabel() {
+        textLabel.isHidden = varient == .dot
+        textLabel.attributedText = getAttributedText()
+    }
+}
+
+extension Badge.Push {
+    private func getAttributedText() -> NSAttributedString? {
+        let attribute: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 9.0, weight: .bold),
+            .foregroundColor: UIColor.alias(.staticWhite),
+            .kern: 0.28,
+            .paragraphStyle: {
+                let style = NSMutableParagraphStyle()
+                style.lineHeightMultiple = 1.07
+                return style
+            }()
+        ]
+        
+        switch varient {
+        case .dot:
+            return nil
+        case .new:
+            return .init(string: "N", attributes: attribute)
+        case .number(let number):
+            return .init(string: "\(number)", attributes: attribute)
+        }
+    }
+}
+
+extension Badge.Push.Varient {
+    var edgeInsets: UIEdgeInsets {
+        switch self {
+        case .dot:
+            return .init(top: 2, left: 2, bottom: 2, right: 2)
+        case .number:
+            return .init(top: 2, left: 5.5, bottom: 2, right: 5.5)
+        case .new:
+            return .init(top: 2, left: 4.5, bottom: 2, right: 4.5)
+        }
+    }
+}
--- Sources/Montage/Element/Badge/Push/PushBadgeController.swift
+++ Sources/Montage/Element/Badge/Push/PushBadgeController.swift
@@ -5,4 +5,43 @@
 //  Created by Euigyom Kim on 2023/04/26.
 //
 
-import Foundation
+import SwiftUI
+import Pretendard
+
+extension Badge {
+    public struct PushBadgeController: UIViewRepresentable {
+        /// 뱃지의 외관입니다.
+        @State public var varient: Badge.Push.Varient = .dot
+        
+        public typealias UIViewType = Badge.Push
+        
+        public func makeUIView(context: Context) -> UIViewType {
+            .init()
+        }
+        
+        public func updateUIView(_ uiView: UIViewType, context: Context) {
+            uiView.varient = varient
+        }
+    }
+}
+
+var pushBadgeControllerPreview: some View {
+    VStack(alignment: .leading, spacing: .spacing(.pt20)) {
+        Badge.PushBadgeController(varient: .dot).fixedSize()
+        
+        Badge.PushBadgeController(varient: .new).fixedSize()
+        
+        Badge.PushBadgeController(varient: .number(1)).fixedSize()
+        
+        Badge.PushBadgeController(varient: .number(999)).fixedSize()
+    }
+}
+
+struct PushBadgeController_Previews: PreviewProvider {
+    static var previews: some View {
+        pushBadgeControllerPreview
+            .padding()
+            .background(SwiftUI.Color(.alias(.backgroundNormal)))
+            .previewLayout(.sizeThatFits)
+    }
+}
