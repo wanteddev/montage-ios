82e61fc28310b9aacdfc4dd871773fb2865d6371
Euigyom Kim <egkim@dehol.kr>
2023-06-08 06:47:45 +0900

    fix: 기존 사이즈 파라미터를 되살림
    Signed-off-by: Euigyom Kim <egkim@dehol.kr>

--- Sources/Montage/Extension/SwiftUI/Text+Montage.swift
+++ Sources/Montage/Extension/SwiftUI/Text+Montage.swift
@@ -10,11 +10,12 @@ import SwiftUI
 public extension Text {
     func montage(
         varient: Typography.Variant = .body1,
+        size: Typography.Size = .small,
         weight: Typography.Weight = .regular,
         color: Color.Alias = .labelNormal
     ) -> Text {
-        font(.montage(varient: varient, weight: weight, size: .small))
-            .tracking(Typography.getTracking(varient: varient, size: .small))
+        font(.montage(varient: varient, weight: weight, size: size))
+            .tracking(Typography.getTracking(varient: varient, size: size))
             .foregroundColor(.alias(color))
     }
 }
