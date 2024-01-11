a032f0662e7db75180fd54109e4840005e352b98
Junghun Seo <junghun@wantedlab.com>
2024-01-11 10:58:14 +0900

    feat: addFill 아이콘 추가

--- Sources/Montage/Foundation/Icon.swift
+++ Sources/Montage/Foundation/Icon.swift
@@ -10,6 +10,7 @@ import Foundation
 /// Montage 번들 내에 포함된 아이콘들의 이름들입니다.
 public enum Icon: String, CaseIterable {
     case add
+    case addFill
     case apps
     case arrowDown
     case arrowLeft
