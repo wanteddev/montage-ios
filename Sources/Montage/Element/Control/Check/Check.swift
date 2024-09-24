4eae57c9a1e2189d12288e021721b4cacac5c902
Samuel Kim <samyeol.kim@wantedlab.com>
2024-09-24 17:25:09 +0900

    feat: Control/Check
    PI-68353

--- Sources/Montage/Element/Control/Check/Check.swift
+++ Sources/Montage/Element/Control/Check/Check.swift
@@ -17,9 +17,15 @@ public protocol CheckControlDelegate: AnyObject {
 extension Control {
     /// 체크 이미지를 표현하는 Control Element 입니다. ``MontageControl``의 일부만을 표현할 수 있습니다.
     public class Check: UIView, MontageControl {
-        private enum Const {
-            static let wrapperBoxSize: CGSize = .init(width: 24, height: 24)
-        }
+        private let boxView = UIView()
+        
+        private lazy var imageView: UIImageView = {
+            let view = UIImageView()
+            view.isUserInteractionEnabled = false
+            view.tintColor = .alias(.labelAssistive)
+            view.image = .montage(.checkThick)
+            return view
+        }()
         
         /// Control Element의 모양을 표현하기 위한 상태값입니다.
         /// `.partial` 상태는 지원하지 않으며, 해당 상태로 설정할 경우 자동으로 `.checked` 상태로 변경후 설정됩니다.
@@ -39,32 +45,23 @@ extension Control {
             }
         }
         
-        private lazy var imageView: UIImageView = {
-            let view = UIImageView()
-            view.isUserInteractionEnabled = false
-            view.tintColor = .alias(.labelAssistive)
-            view.image = .montage(.checkThick)
-            return view
-        }()
+        private let size: MontageControlSize
+        private let interactionView = Decorate.Interaction()
+                
+        private var longPressRecognizer: UILongPressGestureRecognizer?
         
-        private var tapRecognizer: UITapGestureRecognizer?
+        public weak var delegate: CheckControlDelegate?
         
-        private weak var delegate: CheckControlDelegate?
-        
-        /// Check 객체를 생성합니다.
-        /// - Parameter delegate: Check 버튼을 Element 단독으로 사용할 경우 이벤트를 받을 delegate 객체입니다.
-        public init(delegate: CheckControlDelegate? = nil) {
+        public init(size: MontageControlSize) {
+            self.size = size
             super.init(frame: .zero)
-            self.delegate = delegate
             
             setupViews()
             bindEvent()
         }
         
-        override public required init?(coder: NSCoder) {
-            super.init(coder: coder)
-            setupViews()
-            bindEvent()
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
         }
         
         override public func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
@@ -75,38 +72,47 @@ extension Control {
         
         /// Element의 기본적인 사이즈를 정의합니다.
         override public var intrinsicContentSize: CGSize {
-            Const.wrapperBoxSize
+            containerSize
         }
     }
 }
 
 extension Control.Check {
     private func setupViews() {
-        addSubview(imageView)
+        addSubview(boxView)
+        addSubview(interactionView)
         
+        boxView.addSubview(imageView)
+        
+        boxView.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        interactionView.translatesAutoresizingMaskIntoConstraints = false
         
-        widthAnchor.constraint(equalToConstant: Const.wrapperBoxSize.width).isActive = true
-        heightAnchor.constraint(equalToConstant: Const.wrapperBoxSize.height).isActive = true
+        widthAnchor.constraint(equalToConstant: containerSize.width).isActive = true
+        heightAnchor.constraint(equalToConstant: containerSize.height).isActive = true
         
-        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        boxView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: boxInsets.left).isActive = true
+        boxView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -boxInsets.right).isActive = true
+        boxView.topAnchor.constraint(equalTo: topAnchor, constant: boxInsets.top).isActive = true
+        boxView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -boxInsets.bottom).isActive = true
+        
+        imageView.leadingAnchor.constraint(equalTo: boxView.leadingAnchor, constant: imageInsets.left).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: boxView.trailingAnchor, constant: -imageInsets.right).isActive = true
+        imageView.topAnchor.constraint(equalTo: boxView.topAnchor, constant: imageInsets.top).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: boxView.bottomAnchor, constant: -imageInsets.bottom).isActive = true
+        
+        interactionView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        interactionView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        interactionView.widthAnchor.constraint(equalToConstant: interactionSize.width).isActive = true
+        interactionView.heightAnchor.constraint(equalToConstant: interactionSize.height).isActive = true
         
         updateViews()
     }
     
-    private func bindEvent() {
-        guard delegate != nil else { return }
-        let recognizer = UITapGestureRecognizer()
-        recognizer.addTarget(self, action: #selector(tapped))
-        addGestureRecognizer(recognizer)
-        tapRecognizer = recognizer
-    }
-    
     private func updateViews() {
         isUserInteractionEnabled = false == disable
+        
+        boxView.layer.opacity = .opacity(disable ? .p043 : .p100)
         
         switch state {
         case .unchecked:
@@ -115,12 +121,83 @@ extension Control.Check {
             imageView.tintColor = .alias(.primaryNormal)
         }
         
-        if disable {
-            imageView.tintColor = imageView.tintColor.withAlphaComponent(.opacity(.p043))
+        interactionView.variant = .normal
+        interactionView.layer.cornerRadius = interactionSize.width / 2
+    }
+    
+    private func bindEvent() {
+        let longPressRecognizer = UILongPressGestureRecognizer()
+        longPressRecognizer.delegate = self
+        longPressRecognizer.minimumPressDuration = 0
+        longPressRecognizer.cancelsTouchesInView = false
+        longPressRecognizer.addTarget(self, action: #selector(longPressed))
+        addGestureRecognizer(longPressRecognizer)
+        self.longPressRecognizer = longPressRecognizer
+    }
+    
+    @objc private func longPressed() {
+        guard let recognizer = longPressRecognizer else { return }
+        
+        switch recognizer.state {
+        case .began:
+            interactionView.state = .pressed
+        case .changed:
+            // 스크롤 시 버튼이 눌리지 않도록 state를 normal로 변경
+            // 3D touch 모델은 스크롤 하지 않아도 changed가 실행되서 적용하지 않음
+            if traitCollection.forceTouchCapability != UIForceTouchCapability.available {
+                interactionView.state = .normal
+            }
+        case .ended:
+            guard interactionView.state == .pressed else { return }
+            interactionView.state = .normal
+            state = switch state {
+            case .checked: .unchecked
+            case .partial, .unchecked: .checked
+            }
+            delegate?.didTappedCheck(self)
+        default:
+            break
+        }
+    }
+}
+
+extension Control.Check {
+    var containerSize: CGSize {
+        switch size {
+        case .normal:
+            return .init(width: 24, height: 24)
+        case .small:
+            return .init(width: 20, height: 20)
         }
     }
     
-    @objc private func tapped() {
-        delegate?.didTappedCheck(self)
+    var boxInsets: UIEdgeInsets {
+        .zero
+    }
+    
+    var imageInsets: UIEdgeInsets {
+        .zero
+    }
+    
+    var cornerRadius: CGFloat {
+        .zero
+    }
+    
+    var interactionSize: CGSize {
+        switch size {
+        case .normal:
+            return .init(width: 32, height: 32)
+        case .small:
+            return .init(width: 28, height: 28)
+        }
+    }
+}
+
+extension Control.Check: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        return true
     }
 }
