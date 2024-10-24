923e1b907ebed21eb8caa1a7e52370ca9e050bdf
Sanghoon Ahn <ansh0324@gmail.com>
2024-10-24 11:29:20 +0900

    feat: Card 추가

---
+++ Sources/Montage/Components/Card/Card.swift
@@ -0,0 +1,10 @@
+//
+//  Card.swift
+//  Montage
+//
+//  Created by Sanghoon Ahn on 10/22/24.
+//
+
+import SwiftUI
+
+public enum Card { }
---
+++ Sources/Montage/Components/Card/ListCard.swift
@@ -0,0 +1,176 @@
+//
+//  ListCard.swift
+//  Montage
+//
+//  Created by Sanghoon Ahn on 10/24/24.
+//
+
+import SwiftUI
+
+extension Card {
+    public struct List<ImageLoader: View, T: View, C: View, EC: View, TC: View, BC: View, LC: View, RC: View>: View {
+        @Binding private var skeleton: Bool
+        private let imageRatio: Ratio
+        private let imageWidth: CGFloat
+        private let imageLoader: (() -> ImageLoader)
+        private let title: (() -> T)
+        private let caption: (() -> C)
+        private let extraCaption : (() -> EC)
+        private let topContent: (() -> TC)
+        private let bottomContent: (() -> BC)
+        private let leftContent: (() -> LC)
+        private let rightContent: (() -> RC)
+        
+        public init(
+            skeleton: Binding<Bool>,
+            imageRatio: Ratio = .r3x2,
+            imageWidth: CGFloat,
+            @ViewBuilder imageLoader: @escaping (() -> ImageLoader),
+            @ViewBuilder title: @escaping (() -> T) = { EmptyView() },
+            @ViewBuilder caption: @escaping (() -> C) = { EmptyView() },
+            @ViewBuilder extraCaption: @escaping (() -> EC) = { EmptyView() },
+            @ViewBuilder topContent: @escaping (() -> TC) = { EmptyView() },
+            @ViewBuilder bottomContent: @escaping (() -> BC) = { EmptyView() },
+            @ViewBuilder leftContent: @escaping (() -> LC) = { EmptyView() },
+            @ViewBuilder rightContent: @escaping (() -> RC) = { EmptyView() }
+        ) {
+            self._skeleton = skeleton
+            self.imageRatio = imageRatio
+            self.imageWidth = imageWidth
+            self.imageLoader = imageLoader
+            self.title = title
+            self.caption = caption
+            self.extraCaption = extraCaption
+            self.topContent = topContent
+            self.bottomContent = bottomContent
+            self.leftContent = leftContent
+            self.rightContent = rightContent
+        }
+
+        public var body: some View {
+            HStack(alignment: .center, spacing: .zero) {
+                
+                leftContent()
+                    .padding(.trailing, 12)
+                
+                ZStack {
+                    ThumbnailController(
+                        ratio: imageRatio,
+                        portrait: false,
+                        width: imageWidth,
+                        imageLoader: imageLoader
+                    )
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .inset(by: 0.5)
+                        .strokeBorder(SwiftUI.Color.alias(.lineAlternative), lineWidth: 1)
+                )
+                .skeleton(shape: .rectangle, show: $skeleton)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .padding(.trailing, 12)
+
+                VStack(alignment: .leading, spacing: 8) {
+                    topContent()
+                        .skeleton(shape: .rectangle, show: $skeleton)
+                        .clipShape(RoundedRectangle(cornerRadius: 3))
+                    
+                    VStack(alignment: .leading, spacing: .zero) {
+                        
+                        title()
+                            .skeleton(show: $skeleton)
+                        
+                        extraCaption()
+                            .skeleton(show: $skeleton)
+                            .padding(.top, 4)
+
+                        caption()
+                            .skeleton(show: $skeleton)
+                            .padding(.top, 4)
+                    }
+                    
+                    bottomContent()
+                        .skeleton(shape: .rectangle, show: $skeleton)
+                        .clipShape(RoundedRectangle(cornerRadius: 3))
+                }
+                .padding(.trailing, 12)
+                
+                Spacer()
+                
+                rightContent()
+            }
+            .frame(height: imageWidth * (imageRatio.size.height / imageRatio.size.width))
+        }
+    }
+}
+
+import Pretendard
+
+#Preview {
+    let _ = try? Pretendard.registerFonts()
+    Card.List(
+        skeleton: .constant(false),
+        imageWidth: 96,
+        imageLoader: {
+            AsyncImage(
+                url: URL(string:"https://developer.apple.com/xcode/images/xcode-15-hero-large_2x.webp")!
+            ) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                } else {
+                    Image(uiImage: UIImage(named: "placeholder")!)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+        },
+        title: {
+            Text("제목")
+                .montage(variant: .body1, weight: .bold)
+                .paragraph(variant: .body1)
+        },
+        caption: {
+            Text("캡션")
+                .montage(variant: .label2, weight: .medium, alias: .labelAlternative)
+                .paragraph(variant: .label2)
+        },
+        extraCaption: {
+            Text("추가 캡션")
+                .montage(variant: .label2, weight: .medium, alias: .labelAlternative)
+                .paragraph(variant: .label2)
+        },
+        topContent: {
+            HStack {
+                Badge.ContentBadgeController(text: "텍스트")
+                    .fixedSize()
+                Badge.ContentBadgeController(text: "텍스트")
+                    .fixedSize()
+                Badge.ContentBadgeController(text: "텍스트")
+                    .fixedSize()
+                Badge.ContentBadgeController(text: "텍스트")
+                    .fixedSize()
+            }
+        },
+        bottomContent: {
+            HStack {
+                Badge.ContentBadgeController(text: "텍스트")
+                    .fixedSize()
+                Badge.ContentBadgeController(text: "텍스트")
+                    .fixedSize()
+                Badge.ContentBadgeController(text: "텍스트")
+                    .fixedSize()
+            }
+        },
+        leftContent: {
+            Control.CheckboxController(state: .unchecked)
+                .fixedSize()
+        },
+        rightContent: {
+            Image.montage(.chevronRight)
+                .foregroundStyle(SwiftUI.Color.alias(.labelAssistive))
+        }
+    )
+}
+
