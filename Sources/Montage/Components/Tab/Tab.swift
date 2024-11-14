779aeb4c84bffc9c3cd22d1d0ebde184b76427de
Samuel Kim <samyeol.kim@wantedlab.com>
2024-11-14 18:36:46 +0900

    feat: Tab 컴포넌트 제작
    PI-69613

---
+++ Sources/Montage/Components/Tab/Tab.swift
@@ -0,0 +1,246 @@
+//
+//  Tab.swift
+//  Montage
+//
+//  Created by 김삼열 on 11/14/24.
+//
+
+import SwiftUI
+
+public struct Tab: View {
+    // MARK: - Types
+    public enum Resize {
+        case normal, fill
+    }
+    
+    public enum Size {
+        case small, meidum, large
+    }
+
+    // MARK: - Initializer
+    @Binding private var selectedIndex: Int
+    private let items: [String]
+    private let actions: (Int) -> Void
+
+    public init(
+        selectedIndex: Binding<Int>,
+        items: [String],
+        actions: @escaping (Int) -> Void = { _ in }
+    ) {
+        _selectedIndex = selectedIndex
+        self.items = items
+        self.actions = actions
+    }
+    
+    // MARK: - Body
+    @State private var contentOffset: CGPoint = .zero
+    @State private var contentWidth: CGFloat = .zero
+    @State private var scrollViewWidth: CGFloat = .zero
+    @State private var needsLeftGradient = false
+    @State private var needsRightGradient = false
+    
+    private let gradientColors = {
+        [1.0, 0.86, 0.73, 0.62 ,0.52, 0.43, 0.35, 0.29, 0.23, 0.18, 0.14, 0.1, 0.07, 0.04, 0.02, 0.0].map {
+            SwiftUI.Color.black.opacity($0)
+        }
+    }()
+    private let gradientWidth: CGFloat = 48
+    
+    public var body: some View {
+        ZStack(alignment: .bottom) {
+            HStack(spacing: 0) {
+                ScrollViewReader { reader in
+                    HStack(spacing: 20) {
+                        HStack(spacing: 0) {
+                            HStack(spacing: resize == .normal ? 24 : 0) {
+                                ForEach(Array(items.enumerated()), id: \.offset) { index, item in
+                                    ItemView(
+                                        resize: resize,
+                                        size: size,
+                                        isSelected: index == selectedIndex,
+                                        title: item
+                                    )
+                                    .onTapGesture {
+                                        selectedIndex = index
+                                        actions(index)
+                                    }
+                                    .id(index)
+                                }
+                            }
+                            Spacer(minLength: 0)
+                                .if(resize == .normal)
+                        }
+                        .readSize(onChange: {
+                            contentWidth = $0.width
+                        })
+                        .scrollable(.horizontal, contentOffset: $contentOffset)
+                        .readSize(onChange: {
+                            scrollViewWidth = $0.width
+                        })
+                        .mask {
+                            gradientEdge()
+                        }
+                        .onChange(of: selectedIndex) { index in
+                            withAnimation {
+                                reader.scrollTo(index, anchor: .center)
+                            }
+                        }
+                        .onChange(of: contentOffset) { _ in
+                            withAnimation(.easeInOut(duration: 0.2)) {
+                                setNeedsGradientIfNeeded()
+                            }
+                        }
+                        .onChange(of: contentWidth) { _ in
+                            setNeedsGradientIfNeeded()
+                        }
+                    
+                        if let icon, let iconButtonAction {
+                            Button.IconButton(icon: icon) {
+                                iconButtonAction()
+                            }
+                            .fixedSize()
+                        }
+                    }
+                    .padding(.leading, padding ? 20 : 0)
+                    .padding(.trailing, padding ? (icon == nil ? 20 : 16) : 0)
+                }
+            }
+            Rectangle()
+                .fill(SwiftUI.Color.alias(.lineAlternative))
+                .frame(height: 1)
+        }
+    }
+    
+    private func gradientEdge() -> some View {
+        HStack(spacing: 0) {
+            Group {
+                if needsLeftGradient {
+                    LinearGradient(
+                        colors: gradientColors,
+                        startPoint: .init(x: 1, y: 0),
+                        endPoint: .init(x: 0, y: 0)
+                    )
+                } else {
+                    Rectangle()
+                }
+            }
+            .frame(width: gradientWidth)
+            
+            Rectangle()
+                .frame(maxWidth: .infinity)
+            
+            Group {
+                if needsRightGradient {
+                    LinearGradient(
+                        colors: gradientColors,
+                        startPoint: .init(x: 0, y: 0),
+                        endPoint: .init(x: 1, y: 0)
+                    )
+                } else {
+                    Rectangle()
+                }
+            }
+            .frame(width: gradientWidth)
+        }
+        .allowsHitTesting(false)
+    }
+
+    private func setNeedsGradientIfNeeded() {
+        if -contentOffset.x + scrollViewWidth < contentWidth {
+            needsRightGradient = true
+        } else {
+            needsRightGradient = false
+        }
+
+        if contentOffset.x < 0 {
+            needsLeftGradient = true
+        } else {
+            needsLeftGradient = false
+        }
+    }
+    
+    // MARK: - Modifiers
+    private var resize: Resize = .normal
+    private var size: Size = .small
+    private var padding = false
+    private var icon: Icon? = nil
+    private var iconButtonAction: (() -> Void)?
+    
+    public func resize(_ resize: Resize) -> Self {
+        var zelf = self
+        zelf.resize = resize
+        return zelf
+    }
+    
+    public func size(_ size: Size) -> Self {
+        var zelf = self
+        zelf.size = size
+        return zelf
+    }
+    
+    public func padding(_ padding: Bool = true) -> Self {
+        var zelf = self
+        zelf.padding = padding
+        return zelf
+    }
+    
+    public func iconButton(_ icon: Icon, action: @escaping () -> Void) -> Self {
+        var zelf = self
+        zelf.icon = icon
+        zelf.iconButtonAction = action
+        return zelf
+    }
+
+    // MARK: - Inner View
+    fileprivate struct ItemView: View {
+        let resize: Resize
+        let size: Size
+        let isSelected: Bool
+        let title: String
+
+        @State private var itemWidth: CGFloat = 0
+
+        var body: some View {
+            ZStack(alignment: .bottom) {
+                Text(title)
+                    .montage(
+                        variant: itemFontVariant,
+                        weight: .bold,
+                        alias: isSelected ? .labelStrong : .labelAssistive
+                    )
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .readSize(onChange: {
+                        itemWidth = $0.width
+                    })
+                    .frame(height: itemHeight)
+                Divider()
+                    .frame(height: 2)
+                    .background(SwiftUI.Color.alias(.labelStrong))
+                    .opacity(isSelected ? 1.0 : 0.0)
+                    .if(resize == .normal) {
+                        $0.frame(width: itemWidth)
+                    }
+            }
+        }
+    }
+}
+
+// MARK: - Private
+extension Tab.ItemView {
+    var itemHeight: CGFloat {
+        switch size {
+        case .small: 40
+        case .meidum: 48
+        case .large: 56
+        }
+    }
+    
+    var itemFontVariant: Typography.Variant {
+        switch size {
+        case .small: .body2
+        case .meidum: .headline2
+        case .large: .heading2
+        }
+    }
+}
