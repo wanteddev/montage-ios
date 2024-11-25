0cb5ff41df3b86f10b936f722bc4770b65fb3fca
Samuel Kim <samyeol.kim@wantedlab.com>
2024-11-25 19:49:13 +0900

    fix: Avatar 이미지 URL용 인터페이스 추가
    PI-69905

--- Sources/Montage/Element/Avatar/Avatar.swift
+++ Sources/Montage/Element/Avatar/Avatar.swift
@@ -5,6 +5,50 @@
 //  Created by Sanghoon Ahn on 10/18/24.
 //
 
-import Foundation
+import SwiftUI
 
-public enum Avatar {}
+public enum Avatar {
+    public enum Variant {
+        case icon
+        case image(Image)
+        case url(URL?)
+    }
+    
+    public enum Size {
+        case xsmall
+        case small
+        case medium
+        case large
+        case xlarge
+        
+        var imageSize: CGSize {
+            switch self {
+            case .xsmall: .init(width: 16, height: 16)
+            case .small: .init(width: 21.3, height: 21.3)
+            case .medium: .init(width: 26.67, height: 26.67)
+            case .large: .init(width: 32, height: 32)
+            case .xlarge: .init(width: 37.3, height: 37.3)
+            }
+        }
+        
+        var padding: CGFloat {
+            switch self {
+            case .xsmall: 4
+            case .small: 5.3
+            case .medium: 6.67
+            case .large: 8
+            case .xlarge: 9.3
+            }
+        }
+        
+        var componentSize: CGSize {
+            switch self {
+            case .xsmall: .init(width: 24, height: 24)
+            case .small: .init(width: 32, height: 32)
+            case .medium: .init(width: 40, height: 40)
+            case .large: .init(width: 48, height: 48)
+            case .xlarge: .init(width: 56, height: 56)
+            }
+        }
+    }
+}
--- Sources/Montage/Element/Avatar/CompanyAvatar.swift
+++ Sources/Montage/Element/Avatar/CompanyAvatar.swift
@@ -12,58 +12,15 @@ extension Avatar {
     ///
     /// > OnTap을 전달해야 interaction이 활성화 됩니다.
     public struct Company: View {
-        public enum Variant {
-            case icon
-            case image(Image)
-        }
-        
-        public enum Size {
-            case xsmall
-            case small
-            case medium
-            case large
-            case xlarge
-            
-            var imageSize: CGSize {
-                switch self {
-                case .xsmall: .init(width: 16, height: 16)
-                case .small: .init(width: 21.3, height: 21.3)
-                case .medium: .init(width: 26.67, height: 26.67)
-                case .large: .init(width: 32, height: 32)
-                case .xlarge: .init(width: 37.3, height: 37.3)
-                }
-            }
-            
-            var padding: CGFloat {
-                switch self {
-                case .xsmall: 4
-                case .small: 5.3
-                case .medium: 6.67
-                case .large: 8
-                case .xlarge: 9.3
-                }
-            }
-            
-            var componentSize: CGSize {
-                switch self {
-                case .xsmall: .init(width: 24, height: 24)
-                case .small: .init(width: 32, height: 32)
-                case .medium: .init(width: 40, height: 40)
-                case .large: .init(width: 48, height: 48)
-                case .xlarge: .init(width: 56, height: 56)
-                }
-            }
-        }
-
-        private let variant: Variant
-        private let size: Size
+        private let variant: Avatar.Variant
+        private let size: Avatar.Size
         private let onTap: (() -> Void)?
         
         @State private var isPressed: Bool = false
         
         public init(
-            variant: Variant,
-            size: Size,
+            variant: Avatar.Variant,
+            size: Avatar.Size,
             onTap: (() -> Void)? = nil
         ) {
             self.variant = variant
@@ -102,24 +59,41 @@ extension Avatar {
             )
         }
         
+        private func basicImage() -> some View {
+            return Image.montage(.company)
+                .resizable()
+                .frame(width: size.imageSize.width, height: size.imageSize.height)
+                .foregroundStyle(SwiftUI.Color.alias(.staticWhite))
+        }
+        
+        private func borderedImage(_ image: Image) -> some View {
+            return image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: size.componentSize.width, height: size.componentSize.height)
+                .clipShape(RoundedRectangle(cornerRadius: 6))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 6)
+                        .inset(by: 0.5)
+                        .stroke(SwiftUI.Color.alias(.lineAlternative).opacity(0.08), lineWidth: 1)
+                }
+        }
+        
         @ViewBuilder
         private func processedImage() -> some View {
             switch variant {
             case .icon:
-                Image.montage(.company)
-                    .resizable()
-                    .frame(width: size.imageSize.width, height: size.imageSize.height)
-                    .foregroundStyle(SwiftUI.Color.alias(.staticWhite))
+                basicImage()
             case .image(let image):
-                image
-                    .resizable()
-                    .frame(width: size.componentSize.width, height: size.componentSize.height)
-                    .clipShape(RoundedRectangle(cornerRadius: 6))
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 6)
-                            .inset(by: 0.5)
-                            .stroke(SwiftUI.Color.alias(.lineAlternative).opacity(0.08), lineWidth: 1)
+                borderedImage(image)
+            case .url(let url):
+                AsyncImage(url: url) {
+                    if let image = $0.image {
+                        borderedImage(image)
+                    } else {
+                        basicImage()
                     }
+                }
             }
         }
     }
