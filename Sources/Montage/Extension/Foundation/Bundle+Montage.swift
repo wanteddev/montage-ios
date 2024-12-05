01fec574c8400ba84cec8eab7914fc9e0ec5a451
Samuel Kim <samyeol.kim@wantedlab.com>
2024-12-05 12:53:08 +0900

    fix: Bundle.module 추가

---
+++ Sources/Montage/Extension/Foundation/Bundle+Montage.swift
@@ -0,0 +1,19 @@
+//
+//  BundleFinder.swift
+//  Views
+//
+//  Created by 김삼열 on 12/5/24.
+//  Copyright © 2024 WantedLab Inc. All rights reserved.
+//
+
+
+// swiftlint:disable all
+// swift-format-ignore-file
+// swiftformat:disable all
+import Foundation// MARK: - Swift Bundle Accessor for Frameworks
+private class BundleFinder {}
+extension Foundation.Bundle {
+/// Since Lottie is a dynamic framework, the bundle for classes within this module can be used directly.
+static let module = Bundle(for: BundleFinder.self)
+}// swiftlint:enable all
+// swiftformat:enable all
