fd71d5c653ba0bcdfc7adc008cc8a486a679034c
Samuel Kim <samyeol.kim@wantedlab.com>
2024-12-26 21:42:08 +0900

    feat: Progress Tracker 컴포넌트 제작
    https://www.figma.com/design/MK6KmtXBxX7ZkoQXfD9MFH/%EA%B0%9C%EC%84%A0%3A-Components?node-id=4038-11584&t=MKLgCU9arNQMLAmr-4

---
+++ Sources/Montage/3 Component/6 Navigations/ProgressTracker/HorizontalProgressTracker.swift
@@ -0,0 +1,122 @@
+//
+//  HorizontalProgressTracker.swift
+//  Montage
+//
+//  Created by 김삼열 on 12/18/24.
+//
+
+import SwiftUI
+
+extension ProgressTracker {
+    public struct Horizontal: View {
+        @Binding private var progress: Int
+        private let labels: [String]
+        public init(progress: Binding<Int>, labels: [String]) {
+            self._progress = progress
+            self.labels = labels
+        }
+        
+        public init(progress: Binding<Int>, count: Int) {
+            self._progress = progress
+            self.labels = .init(repeating: "", count: count)
+        }
+        
+        @State private var size: CGSize = .zero
+        @State private var stepperSize: CGSize = .zero
+        @State private var textMaxHeight: CGFloat = .zero
+        
+        public var body: some View {
+            ZStack {
+                let lineLength = (size.width - (stepperSize.width * CGFloat(labels.count))) / CGFloat(labels.count - 1)
+                
+                HStack(alignment: .center, spacing: 0) {
+                    ForEach(labels.indices, id: \.self) { index in
+                        let alignment: HorizontalAlignment = .center
+                        VStack(alignment: alignment, spacing: 8) {
+                            HStack(spacing: 0) {
+                                line(before: index)
+                                Stepper(step: index + 1, state: state(at: index))
+                                line(after: index)
+                            }
+                            ZStack {
+                                text(at: index, alignment: textAlignment(from: alignment))
+                                    .frame(height: textMaxHeight, alignment: .top)
+                                text(at: index, alignment: .center)
+                                    .onGeometryChange(for: CGFloat.self, of: { $0.size.height }, action: { textMaxHeight = max(textMaxHeight, $0) })
+                                    .opacity(0)
+                            }
+                            .if(!labels[index].isEmpty)
+                        }
+                    }
+                }
+                
+                Group {
+                    SwiftUI.Color.clear
+                        .fixedSize(horizontal: false, vertical: true)
+                        .onGeometryChange(for: CGSize.self, of: { $0.size }, action: { size = $0 })
+                    Stepper(step: 0, state: state(at: 0))
+                        .onGeometryChange(for: CGSize.self, of: { $0.size }, action: { stepperSize = $0 })
+                }
+                .opacity(0)
+            }
+        }
+        
+        private func text(at index: Int, alignment: TextAlignment) -> some View {
+            Text(labels[index])
+                .montage(variant: .label2, weight: .bold, color: labelColor(at: index))
+                .multilineTextAlignment(alignment)
+                .if(!labels[index].isEmpty)
+        }
+        
+        private func labelColor(at index: Int) -> SwiftUI.Color {
+            switch state(at: index) {
+            case .complete, .inactive:
+                return .alias(.labelAlternative)
+            case .active:
+                return .alias(.labelNormal)
+            }
+        }
+        
+        private func textAlignment(from horizontalAlignment: HorizontalAlignment) -> TextAlignment {
+            switch horizontalAlignment {
+            case .leading: .leading
+            case .trailing: .trailing
+            default: .center
+            }
+        }
+        
+        private func state(at index: Int) -> Stepper.Status {
+            if index < progress - 1 {
+                .complete
+            } else if index > progress - 1 {
+                .inactive
+            } else {
+                .active
+            }
+        }
+        
+        private func line(before index: Int) -> some View {
+            Group {
+                if index > 0 {
+                    Rectangle()
+                        .foregroundStyle(SwiftUI.Color.alias(state(at: index - 1) == .complete ? .primaryNormal : .lineSolidNormal))
+                } else {
+                    SwiftUI.Color.clear
+                }
+            }
+            .frame(height: 1)
+        }
+        
+        private func line(after index: Int) -> some View {
+            Group {
+                if index < labels.count - 1 {
+                    Rectangle()
+                        .foregroundStyle(SwiftUI.Color.alias(state(at: index) == .complete ? .primaryNormal : .lineSolidNormal))
+                } else {
+                    SwiftUI.Color.clear
+                }
+            }
+            .frame(height: 1)
+        }
+    }
+}
---
+++ Sources/Montage/3 Component/6 Navigations/ProgressTracker/VerticalProgressTracker.swift
@@ -0,0 +1,142 @@
+//
+//  VerticalProgressTracker.swift
+//  Montage
+//
+//  Created by 김삼열 on 12/20/24.
+//
+
+
+import SwiftUI
+
+extension ProgressTracker {
+    public struct Vertical: View {
+        public struct StepContent<A: View, C: View>: View {
+            private let label: String
+            private let labelAccessoryView: () -> A
+            private let contentView: () -> C
+            
+            public init(label: String = "", labelAccessoryView: @escaping () -> A = { EmptyView() }, contentView: @escaping () -> C = { EmptyView() }) {
+                self.label = label
+                self.labelAccessoryView = labelAccessoryView
+                self.contentView = contentView
+            }
+
+//            @State private var textHeight: CGFloat = .zero
+            
+            public var body: some View {
+                ZStack(alignment: .topLeading) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        HStack(alignment: .center, spacing: 6) {
+                            text
+//                                .padding(.top, (20 - textHeight) / 2)
+//                            Spacer(minLength: 6)
+                            labelAccessoryView()
+                        }
+                        .frame(height: 20)
+                        .if(!label.isEmpty)
+                        
+                        contentView()
+                    }
+                    
+//                    Text("Dummy")
+//                        .montage(variant: .label2, weight: .bold)
+//                        .onGeometryChange(for: CGFloat.self, of: { $0.size.height }, action: { textHeight = $0 })
+//                        .opacity(0)
+                }
+            }
+            
+            private var status: Stepper.Status = .inactive
+            func status(_ status: Stepper.Status) -> Self {
+                var zelf = self
+                zelf.status = status
+                return zelf
+            }
+            
+            private var text: some View {
+                return Text(label)
+                    .montage(variant: .label2, weight: .bold, color: labelColor)
+                    .lineLimit(1)
+                    .fixedSize()
+            }
+            
+            private var labelColor: SwiftUI.Color {
+                switch status {
+                case .complete, .inactive:
+                    return .alias(.labelAlternative)
+                case .active:
+                    return .alias(.labelNormal)
+                }
+            }
+        }
+        
+        @Binding private var progress: Int
+        private let stepContents: [StepContent<AnyView, AnyView>]
+        public init(progress: Binding<Int>, stepContents: [StepContent<AnyView, AnyView>]) {
+            self._progress = progress
+            self.stepContents = stepContents
+        }
+        
+        @State private var lineLengths: [Int: CGFloat] = [:]
+        @State private var stepperSize: CGSize = .zero
+        
+        public var body: some View {
+            ZStack(alignment: .topLeading) {
+                VStack(spacing: 0) {
+                    ForEach(stepContents.indices, id: \.self) { index in
+                        Group {
+                            if index < stepContents.count - 1 {
+                                line(after: index)
+                            } else {
+                                SwiftUI.Color.clear
+                            }
+                        }
+                        .frame(height: lineLengths[index] ?? .zero)
+                    }
+                }
+                .frame(width: stepperSize.width)
+                
+                VStack(spacing: 0) {
+                    ForEach(stepContents.indices, id: \.self) { index in
+                        HStack(alignment: .top, spacing: 0) {
+                            Stepper(step: index + 1, state: state(at: index))
+                            
+                            if index >= 0 && index < stepContents.count {
+                                stepContents[index]
+                                    .status(state(at: index))
+                                    .padding(.leading, 8)
+                                    
+                            }
+                            
+                            Spacer(minLength: 0)
+                        }
+                        .padding(.bottom, index < stepContents.count - 1 ? 20 : 0)
+                        .onGeometryChange(for: CGFloat.self, of: { $0.size.height }, action: { lineLengths.updateValue($0, forKey: index) })
+                    }
+                }
+                
+                // 사이즈 측정을 위한 투명 뷰들
+                Group {
+                    Stepper(step: 0, state: state(at: 0))
+                        .onGeometryChange(for: CGSize.self, of: { $0.size }, action: { stepperSize = $0 })
+                }
+                .opacity(0)
+            }
+        }
+        
+        private func state(at index: Int) -> Stepper.Status {
+            if index < progress - 1 {
+                .complete
+            } else if index > progress - 1 {
+                .inactive
+            } else {
+                .active
+            }
+        }
+        
+        private func line(after index: Int) -> some View {
+            Rectangle()
+                .frame(width: 1)
+                .foregroundStyle(SwiftUI.Color.alias(state(at: index) == .complete ? .primaryNormal : .lineSolidNormal))
+        }
+    }
+}
