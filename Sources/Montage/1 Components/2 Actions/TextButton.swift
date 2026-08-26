fcb795127e7a80b55018bf043de314ca867ed5f3
Samuel Kim <samyeol.kim@wantedlab.com>
2026-08-26 19:26:26 +0900

    refactor: fill(horizontal:vertical:) 제거하고 fillWidth로 일원화
    Refs: WRP-2503

--- Sources/Montage/1 Components/2 Actions/TextButton.swift
+++ Sources/Montage/1 Components/2 Actions/TextButton.swift
@@ -146,28 +146,21 @@ public struct TextButton: View {
         .init(base: base.loading(loading))
     }
 
-    /// 버튼이 수평 또는 수직 방향으로 공간을 채우도록 설정합니다.
+    /// 버튼이 수평으로 공간을 채우도록 설정합니다.
     ///
     /// 버튼의 크기를 조절하여 컨테이너 뷰의 공간을 효율적으로 활용할 때 사용합니다.
     ///
     /// ```swift
     /// // 부모 뷰의 가로 너비를 모두 채우는 버튼
     /// TextButton(text: "전체 확인")
-    ///     .fill(horizontal: true)
-    ///
-    /// // 가로, 세로 모두 채우는 버튼
-    /// TextButton(text: "영역 전체 채우기")
-    ///     .fill(horizontal: true, vertical: true)
+    ///     .fillWidth(true)
     /// ```
     ///
     /// - Parameters:
-    ///   - fillHorizontal: 수평 방향 채우기 여부, 생략하면 기본값으로 `false` 적용
-    ///   - fillVertical: 수직 방향 채우기 여부, 생략하면 기본값으로 `false` 적용
+    ///   - fillWidth: 채우기 여부, 생략하면 기본값으로 `true` 적용
     /// - Returns: 수정된 버튼 인스턴스
-    public func fill(horizontal fillHorizontal: Bool = false, vertical fillVertical: Bool = false)
-        -> Self
-    {
-        .init(base: base.fill(horizontal: fillHorizontal, vertical: fillVertical))
+    public func fillWidth(_ fillWidth: Bool = true) -> Self {
+        .init(base: base.fillWidth(fillWidth))
     }
 
     /// 뷰의 내용과 동작을 정의합니다.
